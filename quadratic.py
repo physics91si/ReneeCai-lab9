bb464e0c5d4e2dce2dcc66cf36e7db272d27e64c
Renee Cai <reneecai@gmail.com>
2016-04-27 22:20:27 -0700

    lab9

--- quadratic.py
+++ quadratic.py
@@ -6,7 +6,7 @@ import sys
 #Enter values for a, b, and c in the command line
 #e.g. run: >python quadratic.py 1 2 -15
 def main():
-    a = sys.argv[1]
+    a = sys.argv[1] #
     b = sys.argv[2]
     c = sys.argv[3]
     x1, x2 = find_roots(a, b, c)
@@ -20,6 +20,8 @@ def find_roots(a,b,c):
     x1 = (-b + sqrt_mid)/2*a
     x2 = (-b - sqrt_mid)/2*a
     return x1, x2
+
+
 
 
 if __name__=="__main__":
